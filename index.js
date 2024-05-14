7a9744ad5cd0caccb3684a76bc8e8611816e86d4
REGULA SHARATH <100291002+SharathRegula@users.noreply.github.com>
2024-05-14 11:52:18 +0000

    Changed Port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const connectDB = require("./db/connectDB");
 const questionRouter = require("./routes/question");
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8001;
 connectDB;
 
 app.get("/", (req, res) => {
